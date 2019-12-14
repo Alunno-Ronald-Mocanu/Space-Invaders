6467fd68916f1a068f44c0d6fdcdea4d383c3e41
Ronald mocanu <56753380+Alunno-Ronald-Mocanu@users.noreply.github.com>
2019-12-14 10:48:32 +0100

    Aggiunti file java parte 1

---
+++ SpaceInvaders.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spaceinvaders;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+/**
+ *
+ * @author perilli.alberto
+ */
+public class SpaceInvaders extends JFrame {
+
+    public SpaceInvaders() throws InterruptedException {
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); 
+        Intro intro = new Intro();
+        this.add(intro);
+        // impacchetto il tutto e rendi visibile
+        this.pack();
+        this.setVisible(true);
+        // avvia l'introduzione
+        intro.run();    
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+    }
+    
+}
